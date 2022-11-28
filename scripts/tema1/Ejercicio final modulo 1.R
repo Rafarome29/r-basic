dfca12b7bc401d0cde6ebb586777175d48554b7e
Rafarome29 <rafaromegil@gmail.com>
2022-11-28 13:06:18 +0100

    Create Ejercicio final modulo 1.R

---
+++ scripts/tema1/Ejercicio final modulo 1.R
@@ -0,0 +1,37 @@
+250000000/60 #calculamos minutos 4.166.667
+
+4166667%/%60 #calculamos horas 69.444 horas
+4166667%%60 #calculamos el resto de las horas 27 minutos
+
+
+#calculamos los dias
+69444%/%24  #2.893 días
+69444%%24   #12 horas de resto
+
+#calculamos el número de dias y años
+2893%/%365 #7 años (dos bisiestos)
+2893%%365
+
+2018+7 #2025
+
+#hay dos años bisiestos
+
+(2893%%365)-2 #336 días
+
+#Suma nos daria que estamos a 2 de Diciembre de 2025 a las 12:27
+
+
+
+Ecu1grado = function(A, B){-B/A}
+
+Ecu1grado(5,3) #-0,6
+Ecu1grado(7,-14) #2
+Ecu1grado(1,-1) #1
+
+
+round(3*exp(1)-pi,3)
+#5,013
+
+
+round(Mod((2+3i)^2/(5+8i)),3)
+#1,378
